4e6f10e8729d5c149b92286b2a3796975c3fd4d7
raginideotare <deotare.ragini@gmail.com>
2026-08-25 18:16:09 +0530

    Assignment 1

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,8 @@
+ console.log("Execute the first line");
+ console.log("Execute the second line");
+ console.log("Execute the third line");
+ a=(5+10);
+ console.log("Execute the fourth line");
+ console.log("Execute the fifth line");
+ console.log("Execute the sixth line");
+ console.log("Execute the seventh line");
---
+++ javascript-training/language-basics/git-commands.js
@@ -0,0 +1,11 @@
+ // 1.git init--> to monitor all the changes in the project
+
+ // 2.git remote add origin https://github.com/RaginiDeotare/assignments.git
+
+ // 3.git config --global user.name "raginideotare"
+
+ // 4.git config --global user.email "deotare.ragini@gmail.com" 
+
+ // 5.git add . --> to add all the changes in the staging area
+
+ // 6.git commit -m "message" --> to commit the changes in local repository
